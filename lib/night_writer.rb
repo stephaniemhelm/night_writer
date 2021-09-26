9c8d191c9943bdcfb0cb07ab2badc56ecad5bbed
Stephanie Helm <stephaniemhelm@gmail.com>
2021-09-26 09:30:20 -0600

    refactor night rider

--- lib/night_writer.rb
+++ lib/night_writer.rb
@@ -1,8 +1,23 @@
+class NightWriter
 
+  attr_reader   :input_file
+                :output_file
+
+  def initialize
+    @input_file   = ARGV
+    @output_file  = #2nd file???)
+  end
+
+end
 
 input_array = ARGV
+
+
 
 string = `cat #{input_array[0]} | wc -m`
 characters = string.lstrip.chomp!.to_i
 
 puts "Created #{input_array[1]} containing #{characters} characters"
+
+
+# to run in terminal: ruby lib/night_writer.rb message.txt braille.txt
